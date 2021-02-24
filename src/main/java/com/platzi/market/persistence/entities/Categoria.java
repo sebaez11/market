103bf146f14d9701e81d2e62b601842652ef6111
Sebastian Baez <sebaez200011@gmail.com>
2021-02-24 15:15:46 -0500

    'Cliente' and 'Categoria' entities implementation

---
+++ src/main/java/com/platzi/market/persistence/entities/Categoria.java
@@ -0,0 +1,46 @@
+package com.platzi.market.persistence.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @Column(name = "id_categoria")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCategoria;
+
+    private String descripcion;
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/platzi/market/persistence/entities/Cliente.java
@@ -0,0 +1,24 @@
+package com.platzi.market.persistence.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+    private String apellidos;
+    private Integer celular;
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+}
