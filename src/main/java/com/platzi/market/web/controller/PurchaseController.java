d80e1edf27333b7fb434a0482dc0317a82f197d2
Sebastian Baez <sebaez200011@gmail.com>
2021-03-08 16:00:38 -0500

    Purchase service and controller implemented

---
+++ src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -0,0 +1,45 @@
+package com.platzi.market.web.controller;
+
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.domain.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Purchase>> getAll(){
+        ResponseEntity response = new ResponseEntity<>(purchaseService.getAll() , HttpStatus.OK);
+        return response;
+    }
+
+    @GetMapping("/client/{id}")
+    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("id") String clientId){
+        return purchaseService.getByClient(clientId)
+                .map(purchases -> new ResponseEntity<>(purchases , HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase){
+        ResponseEntity response = new ResponseEntity(purchaseService.save(purchase) , HttpStatus.CREATED);
+        return response;
+    }
+
+}
