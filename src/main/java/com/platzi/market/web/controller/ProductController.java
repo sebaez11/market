9aeaf9cbd25afff92394ce4f2aa1f4e43a5946b1
Sebastian Baez <sebaez200011@gmail.com>
2021-03-08 17:37:42 -0500

    Security layer to API access was implemented

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,13 +30,21 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200 , message = "Everything is OK")
     public ResponseEntity<List<Product>> getAll(){
         ResponseEntity response = new ResponseEntity<>(productService.getAll() , HttpStatus.OK);
         return response;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+    @ApiOperation("Search a product by id")
+    @ApiResponses({
+            @ApiResponse(code = 200 , message = "OK"),
+            @ApiResponse(code = 404 , message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(@ApiParam(value = "The product id" , required = true , example = "7")
+                                                  @PathVariable("id") int productId){
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product , HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
